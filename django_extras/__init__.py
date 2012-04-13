b3b000dee65b456f1a06924b6542f3c317798407
Tim Savage <tim.savage@jooceylabs.com>
2012-04-13 15:20:50 +1000

    Fixed version number, switched to Beta status as this library is in pre-production

--- django_extras/__init__.py
+++ django_extras/__init__.py
@@ -1,4 +1,5 @@
 # Copyright 2011 Tim Savage <tim.savage@poweredbypenguins.org>
 # Licensed under the terms of the BSD License (see LICENSE)
 
-__version__ = '0.1.1'
+VERSION = (0, 1, 1, 'beta')
+__version__ = '.'.join(map(str, VERSION))
