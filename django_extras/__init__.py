80b8773214f871a4c5b7f307a9b3896aa04289fe
Tim Savage <tim.savage@jooceylabs.com>
2012-04-13 15:19:20 +1000

    Added staff required decorator and usefull http response objects. Bumped version number

--- django_extras/__init__.py
+++ django_extras/__init__.py
@@ -1,5 +1,4 @@
 # Copyright 2011 Tim Savage <tim.savage@poweredbypenguins.org>
 # Licensed under the terms of the BSD License (see LICENSE)
 
-VERSION = (0, 1, 0, 'alpha')
-__version__ = '.'.join(map(str, VERSION))
+__version__ = '0.1.1'
---
+++ django_extras/http/__init__.py
@@ -0,0 +1,42 @@
+from django.core.serializers import json
+from django.http import HttpResponse
+
+
+class HttpResponseUnAuthorised(HttpResponse):
+    status_code = 401
+
+class HttpResponseConflict(HttpResponse):
+    status_code = 409
+
+class HttpResponseNotImplemented(HttpResponse):
+    status_code = 501
+
+class HttpResponseGatewayTimeout(HttpResponse):
+    status_code = 504
+
+
+class FileResponse(HttpResponse):
+    """
+    Response object that handles files
+    """
+    def __init__(self, content, content_type, include_last_modified=True):
+        if isinstance(content, basestring):
+            f = file(content, 'rb')
+        else:
+            f = content
+        super(FileResponse, self).__init__(f, content_type=content_type)
+
+        if include_last_modified:
+            modified = os.path.getmtime(f.name)
+            self['Last-Modified'] = format_http_date(timeval=modified, localtime=True)
+
+
+class JsonResponse(HttpResponse):
+    """
+    Response object that handles JSON encoding and sets the correct content type.
+    """
+    def __init__(self, data, content_type='application/json', **kwargs):
+        super(JsonResponse, self).__init__(
+            simplejson.dumps(data, cls=json.DjangoJSONEncoder),
+            content_type=content_type, **kwargs)
+            
