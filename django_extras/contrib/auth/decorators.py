7625798be333e254ce907ee9c321ebf2880c9003
Tim Savage <tim.savage@poweredbypenguins.org>
2012-06-14 16:38:53 +1000

    Updated decorators to follow Django 1.4 style. Added initial work on documentation.
    - Documentation covers django_extras.contrib.auth.decorators.

--- django_extras/contrib/auth/decorators.py
+++ django_extras/contrib/auth/decorators.py
@@ -1,30 +1,29 @@
 from django.contrib.auth.decorators import *
+from django.core.exceptions import PermissionDenied
 
 
-def superuser_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
+def superuser_required(login_url=None, raise_exception=False):
     """
     Decorator for views that checks that the user is a superuser, redirecting to
     the log-in page if necessary.
     """
-    actual_decorator = user_passes_test(
-        lambda u: u.is_superuser,
-        login_url=login_url,
-        redirect_field_name=redirect_field_name
-    )
-    if function:
-        return actual_decorator(function)
-    return actual_decorator
+    def check_permission(user):
+        if user.is_superuser:
+            return True
+        if raise_exception:
+            raise PermissionDenied
+        return False
+    return user_passes_test(check_permission, login_url=login_url)
 
-def staff_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
+def staff_required(include_superusers=True, login_url=None, raise_exception=False):
     """
     Decorator for views that checks that the user is a staff member, redirecting
     to the log-in page if necessary.
     """
-    actual_decorator = user_passes_test(
-        lambda u: u.is_staff,
-        login_url=login_url,
-        redirect_field_name=redirect_field_name
-    )
-    if function:
-        return actual_decorator(function)
-    return actual_decorator
+    def check_permission(user):
+        if user.is_staff or (include_superusers and user.is_superuser):
+            return True
+        if raise_exception:
+            raise PermissionDenied
+        return False
+    return user_passes_test(check_permission, login_url=login_url)
