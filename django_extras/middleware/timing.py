7bbaf0a695464532239276ba02dd3de0d5f22ecf
Tim Savage <tim.savage@poweredbypenguins.org>
2012-06-16 13:34:37 +1000

    Corrected issue with timing middleware (process_response should always return a response object)

--- django_extras/middleware/timing.py
+++ django_extras/middleware/timing.py
@@ -7,13 +7,16 @@ class TimingMiddleware(object):
 
     This value is the total time spent processing a user request in microseconds.
     """
+    REQUEST_ATTR = '_timing_start'
 
     def process_request(self, request):
-        setattr(request, 'start', datetime.datetime.now())
+        setattr(request, self.REQUEST_ATTR, datetime.datetime.now())
 
     def process_response(self, request, response):
-        start = getattr(request, 'start')
+        start = getattr(request, self.REQUEST_ATTR)
         if start:
             end = datetime.datetime.now()
             length = end - start
             response['X-PROCESSING_TIME_MS'] = str(length.microseconds)
+        return response
+    
