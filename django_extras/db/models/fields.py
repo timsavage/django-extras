f80acf6d5d201d46daef52fc8f90dadb6928dfc8
Tim Savage <tim.savage@poweredbypenguins.org>
2012-06-18 15:08:39 +1000

    Added Percent field, modified existing fields to behave more like built in Django fields. Added documentation.

--- django_extras/db/models/fields.py
+++ django_extras/db/models/fields.py
@@ -1,4 +1,5 @@
 from django.core import exceptions
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django_extras import forms
@@ -6,28 +7,36 @@ from django_extras.core import validators
 from django_extras.core.types import Money
 
 
+_MODULE_NAME = 'django_extras.db.models.fields.'
+
+
 class ColorField(models.CharField):
     """
     Database field that represents a color value.
     """
-    default_error_messages = {
-        'invalid': _(u'This value must be a CSS color value.'),
-    }
+    default_validators = [validators.validate_color]
     description = _("Color value")
 
-    def __init__(self, verbose_name=None, name=None, max_length=30, **kwargs):
-        kwargs.setdefault('verbose_name', verbose_name)
-        kwargs.setdefault('name', name)
-        kwargs.setdefault('max_length', max_length)
-        super(ColorField, self).__init__(**kwargs)
-        self.validators.append(validators.validate_color)
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = kwargs.get('max_length', 75)
+        super(ColorField, self).__init__(*args, **kwargs)
 
     def formfield(self, **kwargs):
+        # As with CharField, this will cause color validation to be performed
+        # twice.
         defaults = {
-            'widget': forms.ColorPickerWidget
+            'form_class': forms.ColorPickerWidget,
         }
         defaults.update(kwargs)
         return super(ColorField, self).formfield(**defaults)
+
+    def south_field_triple(self):
+        """
+        Support for South
+        """
+        from south.modelsinspector import introspector
+        args, kwargs = introspector(self)
+        return _MODULE_NAME + self.__class__.__name__, args, kwargs
 
 
 class MoneyField(models.DecimalField):
@@ -39,8 +48,10 @@ class MoneyField(models.DecimalField):
     }
     description = _("Monetary amount")
 
-    def __init__(self, verbose_name=None, name=None, max_digits=20, decimal_places=4, **kwargs):
-        super(MoneyField, self).__init__(verbose_name, name, max_digits, decimal_places, **kwargs)
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('max_digits', 20)
+        kwargs.setdefault('decimal_places', 4)
+        super(MoneyField, self).__init__(*args, **kwargs)
 
     def to_python(self, value):
         if value is None:
@@ -62,3 +73,26 @@ class MoneyField(models.DecimalField):
         }
         defaults.update(kwargs)
         return super(MoneyField, self).formfield(**defaults)
+
+    def south_field_triple(self):
+        """
+        Support for South
+        """
+        from south.modelsinspector import introspector
+        args, kwargs = introspector(self)
+        return _MODULE_NAME + self.__class__.__name__, args, kwargs
+
+
+class PercentField(models.FloatField):
+    """
+    Float field that ensures field value is in the range 0-100.
+    """
+    default_validators = [
+        MinValueValidator(0),
+        MaxValueValidator(100),
+    ]
+
+    def south_field_triple(self):
+        from south.modelsinspector import introspector
+        args, kwargs = introspector(self)
+        return _MODULE_NAME + self.__class__.__name__, args, kwargs
