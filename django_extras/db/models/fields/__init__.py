0ce57984ab186d2bf328dee2253753860dd5bdee
tim.savage@poweredbypenguins.org <tim.savage@poweredbypenguins.org>
2012-08-04 15:55:15 +1000

    Added JSON field

--- django_extras/db/models/fields/__init__.py
+++ django_extras/db/models/fields/__init__.py
@@ -1,12 +1,11 @@
 from django.core import exceptions
-from django.core.serializers.json import DjangoJSONEncoder
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.utils import simplejson
 from django_extras import forms
 from django_extras.core import validators
 from django_extras.core.types import Money
+from django_extras.db.models.fields.json import JsonField
 
 
 class ColorField(models.CharField):
@@ -39,7 +38,7 @@ class MoneyField(models.DecimalField):
     """
     default_error_messages = {
         'invalid': _(u'This value must be a monetary amount.'),
-    }
+        }
     description = _("Monetary amount")
 
     def __init__(self, *args, **kwargs):
@@ -69,51 +68,34 @@ class PercentField(models.FloatField):
     default_validators = [
         MinValueValidator(0),
         MaxValueValidator(100),
-    ]
+        ]
 
 
-class JsonField(models.TextField):
+class LatitudeField(models.FloatField):
+    """Latitude field
+
+    Ensures value is in hte range (-90)-90.
     """
-    Field that stores a dictionary in the database as JSON.
-    """
-    description = _("JSON dictionary")
-
-    __metaclass__ = models.SubfieldBase
-
     def __init__(self, *args, **kwargs):
-        self.dump_options = { 'cls': DjangoJSONEncoder, }
-        self.load_options = {}
-        self.dump_options.update(kwargs.pop('dump_options', {}))
-        self.load_options.update(kwargs.pop('load_options', {}))
-        super(JsonField, self).__init__(*args, **kwargs)
-
-    def to_python(self, value):
-        if isinstance(value, basestring):
-            try:
-                return simplejson.loads(value, **self.load_options)
-            except ValueError:
-                pass
-        return value
-
-    def get_db_prep_value(self, value, connection, prepared=False):
-        if isinstance(value, basestring):
-            return value
-        return simplejson.dumps(value, **self.dump_options)
-
-    def value_to_string(self, obj):
-        return super(JsonField, self).value_to_string(obj)
-
-    def value_from_object(self, obj):
-        return simplejson.dumps(super(JsonField, self).value_from_object(obj))
-
-    def formfield(self, **kwargs):
-        defaults = {'form_class': forms.JsonField}
-        defaults.update(kwargs)
-        return super(JsonField, self).formfield(**defaults)
+        kwargs.setdefault('validators', [
+            validators.MinValueValidator(-90.0),
+            validators.MaxValueValidator(90.0),
+            ])
+        super(LatitudeField, self).__init__(*args, **kwargs)
 
 
-# Notice: This has been removed for the time being as there are not suitable
-#
+class LongitudeField(models.FloatField):
+    """Longitude field
+
+    Ensures value is in hte range (-180)-180.
+    """
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('validators', [
+            validators.MinValueValidator(-180.0),
+            validators.MaxValueValidator(180.0),
+            ])
+        super(LongitudeField, self).__init__(*args, **kwargs)
+
 # Register special admin widgets if admin is in use.
 #if 'django.contrib.admin' in settings.INSTALLED_APPS:
 #    from django.contrib.admin import options
@@ -122,10 +104,10 @@ class JsonField(models.TextField):
 # Register fields with south.
 try:
     from south.modelsinspector import add_introspection_rules
-    add_introspection_rules([], ["^django_extras\.db\.models\.fields\.ColorField"])
-    add_introspection_rules([], ["^django_extras\.db\.models\.fields\.MoneyField"])
-    add_introspection_rules([], ["^django_extras\.db\.models\.fields\.PercentField"])
-    add_introspection_rules([], ["^django_extras\.db\.models\.fields\.JsonField"])
+    add_introspection_rules([], ["^django_extras\.db\.models\.fields\.\w+Field"])
+#    add_introspection_rules([], ["^django_extras\.db\.models\.fields\.MoneyField"])
+#    add_introspection_rules([], ["^django_extras\.db\.models\.fields\.PercentField"])
+#    add_introspection_rules([], ["^django_extras\.db\.models\.fields\.LatitudeField"])
+#    add_introspection_rules([], ["^django_extras\.db\.models\.fields\.LongitudeField"])
 except ImportError:
     pass
-
---
+++ django_extras/db/models/fields/json.py
@@ -0,0 +1,83 @@
+from decimal import Decimal
+from django.core.serializers.json import DjangoJSONEncoder
+from django.conf import settings
+from django.db import models
+from django.utils import simplejson
+from django_extras import forms
+
+
+def dumps(value):
+    return DjangoJSONEncoder().encode(value)
+
+
+def loads(txt):
+    value = simplejson.loads(
+        txt,
+        parse_float=Decimal,
+        encoding=settings.DEFAULT_CHARSET
+    )
+    return value
+
+
+class JsonDict(dict):
+    """
+    Hack so repr() called by dumpdata will output JSON instead of
+    Python formatted data.  This way fixtures will work!
+    """
+    def __repr__(self):
+        return dumps(self)
+
+
+class JsonList(list):
+    """
+    As above
+    """
+    def __repr__(self):
+        return dumps(self)
+
+
+class JsonField(models.TextField):
+    """Field that serializes/de-serializes a python list/dictionary to the
+    database seamlessly."""
+
+    # Used so to_python() is called
+    __metaclass__ = models.SubfieldBase
+
+    def __init__(self, *args, **kwargs):
+        if 'default' not in kwargs:
+            kwargs['default'] = '{}'
+        models.TextField.__init__(self, *args, **kwargs)
+
+    def to_python(self, value):
+        """Convert our string value to JSON after we load it from the DB"""
+        if value is None or value == '':
+            return {}
+        elif isinstance(value, basestring):
+            res = loads(value)
+            if isinstance(res, dict):
+                return JsonDict(**res)
+            else:
+                return JsonList(res)
+        else:
+            return value
+
+    def get_db_prep_save(self, value, connection):
+        """Convert our JSON object to a string before we save"""
+        if not isinstance(value, (list, dict)):
+            return super(JsonField, self).get_db_prep_save("", connection=connection)
+        else:
+            return super(JsonField, self).get_db_prep_save(dumps(value),
+                connection=connection)
+
+    def formfield(self, **kwargs):
+        defaults = {'form_class': forms.JsonField}
+        defaults.update(kwargs)
+        return super(JsonField, self).formfield(**defaults)
+
+
+# Register field with south.
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^django_extras\.db\.models\.fields\.json\.JsonField"])
+except ImportError:
+    pass
