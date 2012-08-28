7ffd1b8eaa8d64343bd8c63f06685698634a1e67
Tim Savage <tim.savage@poweredbypenguins.org>
2012-08-28 10:18:36 +1000

    Corrected test case

--- django_extras/tests/http/responses.py
+++ django_extras/tests/http/responses.py
@@ -31,5 +31,5 @@ class JsonResponseTestCase(test.TestCase):
         })
 
         actual = ''.join(target)
-        self.assertEqual(actual, '{"foo": "bar", "eek": "2012-06-25T11:09:48"}')
-        self.assertEqual(target['Content-Type'], 'application/json')
+        self.assertEqual('{"foo": "bar", "eek": "2012-06-25 11:09:48"}', actual)
+        self.assertEqual('application/json', target['Content-Type'])
